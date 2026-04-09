dccda9a648af943c54173d1933d74853b6c51e1f
RPEngine <Pjcarter4252@gmail.com>
2026-04-08 20:53:07 -0400

    Add API routes to the server

--- WykedSamuraiGuild/server/server.js
+++ WykedSamuraiGuild/server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import apiRoutes from "./routes/apiRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", apiRoutes);
 
 app.get("/", (req, res) => {
   res.send("WSG backend is running");
